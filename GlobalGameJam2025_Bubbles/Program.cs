30997f06b684fbc1d51b32c0bcab56524a44415b
Boris Sedov <borissedov@gmail.com>
2025-01-30 22:42:05 +0400

    Redirects fix

--- GlobalGameJam2025_Bubbles/Program.cs
+++ GlobalGameJam2025_Bubbles/Program.cs
@@ -45,7 +45,7 @@ if (!app.Environment.IsDevelopment())
 app.UseAuthentication();
 app.UseAuthorization();
 
-// app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
